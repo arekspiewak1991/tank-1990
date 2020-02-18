c5752c9bdbae5d04c126464077294b75423d7018
Arkadiusz Śpiewak <arekspiewak1991@gmail.com>
2020-02-18 11:56:08 +0100

    User tank movement

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
 import gameplay.Gameplay;
 
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame {
+    private static int WINDOW_WIDHT = 1200;
+    private static int WINDOW_HEIGHT = 900;
+
     public static void main(String[] args) {
         Gameplay gameplay = new Gameplay();
-        gameplay.init(1100, 900);
+        JFrame jFrame = new JFrame("Tank 1990");
+        jFrame.setBounds(10, 10, WINDOW_WIDHT, WINDOW_HEIGHT);
+        jFrame.setBackground(Color.DARK_GRAY);
+        jFrame.setResizable(false);
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.add(gameplay);
     }
 }
--- src/tank/UserTank.java
+++ src/tank/UserTank.java
@@ -1,14 +1,26 @@
 package tank;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class UserTank extends Tank {
     private int initialX = 200;
-    private int initialY = 100;
+    private int initialY = 200;
     private TankType type = TankType.Small;
+    private BufferedImage image;
 
-    public UserTank(String name) {
+    public UserTank(String name) throws IOException {
+        image = ImageIO.read(new File("src/tank/tank_user_1.png"));
         this.setName(name);
         this.setPosX(initialX);
         this.setPosY(initialY);
         this.setType(type);
+    }
+
+    public void drawTank(Graphics g) {
+        g.drawImage(image, posX, posY, 50,50, null);
     }
 }
